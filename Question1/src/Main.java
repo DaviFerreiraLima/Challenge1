86afefe272d1a02ac4e2bb3e3e472f9a390a00cb
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-08 17:16:35 -0300

    Adding the main method for the Question 1

--- Question1/src/Main.java
+++ Question1/src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("How manny employees do you want to add ?");
+        int employeesNumber = scanner.nextInt();
+
+        scanner.nextLine(); //Consumir quebra de Linha
+
+        Employee[] employees= new Employee[employeesNumber];
+
+        for (int i =0 ; i<employees.length; i++){
+
+            System.out.println("Write the name of the "+(i+1)+"º employee");
+            String employeeName = scanner.nextLine();
+
+            System.out.println("Write the salary of the "+(i+1)+"º employee");
+            double employeeSalary = scanner.nextDouble();
+
+            scanner.nextLine();//Consumir quebra de Linha
+
+            employees[i] = new Employee(employeeName,employeeSalary);
+
+        }
+
+        for (Employee employe : employees) {
+            System.out.println("Name: "+employe.getName() + ", salário: "+employe.getSalary());
         }
     }
 }
