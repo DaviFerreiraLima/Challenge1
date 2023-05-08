07d2dcb2418df761f9f912fbcadd72d623df0a68
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-08 17:15:51 -0300

    Adding employee class with salary treatment

---
+++ Question1/src/Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+
+    private String name;
+    private double salary;
+
+    public Employee(){
+
+    }
+    public Employee(String name, double salary) {
+        this.name = name;
+        setSalary(salary);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary <= 1000){
+            this.salary = salary + (salary * 0.2);
+        }
+        else if( salary > 1000 && salary <= 2000 ){
+            this.salary = salary + (salary * 0.1);
+
+        }else if( salary > 2000 ){
+            this.salary = salary - (salary * 0.1);
+        }
+    }
+
+
+}
