ec3c98d8d6818eda582c62c2265d087aa06fe221
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-09 18:23:03 -0300

    Adding the Question 2 solved

--- Question2/src/Main.java
+++ Question2/src/Main.java
@@ -1,17 +1,51 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Write the phrase");
+        String phrase = scanner.nextLine();
+        if(phrase instanceof String){
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int happyRecorrencies = 0;
+            int sadRecorrencies =  0;
+
+            result(happyRecorrencies,sadRecorrencies,phrase.toLowerCase());
+            
+        }
+    }
+
+    public static int countRecorrencies(String phrase, String symbol){
+         int recorrencies = 0;
+
+         int position = 0;
+
+         while ((position = phrase.indexOf(symbol,position)) != -1){
+             recorrencies++;
+             position += symbol.length();
+         }
+
+        return recorrencies;
+    }
+
+    public static void result(int happyRecorrencies, int sadRecorrencies, String phrase){
+        String happySymbol = ":-)";
+        String sadSymbol = ":-(";
+
+        happyRecorrencies = countRecorrencies(phrase,happySymbol);
+        sadRecorrencies = countRecorrencies(phrase,sadSymbol);
+
+        if (happyRecorrencies>sadRecorrencies){
+            System.out.println("fun");
+        } else if (happyRecorrencies<sadRecorrencies) {
+            System.out.println("upset");
+        }
+        else {
+            System.out.println("neutral");
         }
     }
 }
