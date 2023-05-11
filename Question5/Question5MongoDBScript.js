c5e44c2678380094f73cbb9a9d65880682d54402
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-11 18:34:14 -0300

    Adding the insert.person command

---
+++ Question5/Question5MongoDBScript.js
@@ -0,0 +1,17 @@
+db.Person.insertOne({
+    "ObjectId" : "",
+    "name": "",
+    "age": "",
+    "phone": "",
+    "height": "",
+    "email": "",
+    "cpf": "",
+    "birthday": "",
+    "address": {
+        "zip": "",
+        "street": "",
+        "complement": "",
+        "state": "",
+        "country": ""
+    }
+})
