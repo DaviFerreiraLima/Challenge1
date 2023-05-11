7e7c1c9df1c68a7025083e9e6010d3b62625dbed
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-11 16:27:02 -0300

    cleaning more the code with the To string alteration

--- Question2/src/MoodIdentifier.java
+++ Question2/src/MoodIdentifier.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MoodIdentifier {
-    static Scanner scanner = new Scanner(System.in);
+
     private String mood;
 
     public MoodIdentifier() {}
@@ -16,6 +16,8 @@ public class MoodIdentifier {
     }
 
     public String askThePhrase() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Write the phrase: ");
         String phrase = scanner.nextLine();
         if (phrase.isBlank()) {
@@ -24,7 +26,7 @@ public class MoodIdentifier {
         return phrase;
     }
 
-    public int countOcorrencies (String phrase, String symbol){
+    public int countMoodOcorrencies (String phrase, String symbol){
         int recorrencies = 0;
 
         int position = 0;
@@ -42,32 +44,30 @@ public class MoodIdentifier {
             String happySymbol = ":-)";
             String sadSymbol = ":-(";
 
-            int happyRecorrencies = countOcorrencies(phrase, happySymbol);
-            int sadRecorrencies = countOcorrencies(phrase, sadSymbol);
+            int happyOcorrencies = countMoodOcorrencies(phrase, happySymbol);
+            int sadOcorrencies = countMoodOcorrencies(phrase, sadSymbol);
 
-            if (happyRecorrencies > sadRecorrencies) {
+            if (happyOcorrencies > sadOcorrencies) {
                 setMood("fun");
-            } else if (happyRecorrencies < sadRecorrencies) {
+            } else if (happyOcorrencies < sadOcorrencies) {
                 setMood("upset");
             } else {
                 setMood("neutral");
             }
 
         }
+
     public void moodProcess () {
         try {
             String phrase = askThePhrase();
             moodIdentifier(phrase.toLowerCase());
-            System.out.println(toString());
+            System.out.println(this.mood);
         }catch (InputMismatchException exception) {
             System.out.println(exception.getMessage());
             System.out.println("Restarting the app\n");
             moodProcess();
         }
            }
-    @Override
-    public String toString() {
-        return mood;
-    }
+
 }
 
