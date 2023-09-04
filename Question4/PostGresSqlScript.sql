9012f38bd7e7704a3e68f7310b12f3ef92735a18
DFL <82511534+DaviFerreiraLima@users.noreply.github.com>
2023-09-04 17:52:31 -0300

    Update PostGresSqlScript.sql

--- Question4/PostGresSqlScript.sql
+++ Question4/PostGresSqlScript.sql
@@ -1,6 +1,3 @@
-create database Question4
-use Question4
-
 create table Address (
 		addressId SERIAL PRIMARY KEY,
 		zip  VARCHAR(50) NOT NULL,
@@ -23,4 +20,24 @@ create table Person (
 	birthday DATE NOT NULL,	
 	address_id SERIAL,
 	FOREIGN KEY (address_id) REFERENCES Address (addressId)
-);
+); 
+
+INSERT INTO Address (zip, street, complement, neighborhood, city, state, country)
+VALUES
+    ('12345-678', 'Rua Principal', 'Apt 101', 'Centro', 'Cidade A', 'Estado A', 'País A'),
+    ('54321-876', 'Avenida Secundária', 'Casa 42', 'Bairro B', 'Cidade B', 'Estado B', 'País B'),
+    ('98765-432', 'Rua dos Fundos', NULL, 'Subúrbio', 'Cidade C', 'Estado C', 'País C');
+
+
+INSERT INTO Person (name, age, phone, height, email, cpf, birthday, address_id)
+VALUES
+    ('João', 30, '(11) 1234-5678', 1.75, 'joao@example.com', '12345678901', '1992-05-15', 1),
+    ('Maria', 25, '(22) 9876-5432', 1.60, 'maria@example.com', '98765432109', '1997-03-20', 2),
+    ('Carlos', 35, '(33) 4444-3333', 1.80, 'carlos@example.com', '55555555505', '1987-11-10', 3);
+
+
+SELECT p.person_id, p.name, p.email, p.cpf, p.birthday, a.city, a.state, a.country
+FROM
+    Person p
+INNER JOIN
+    Address a ON p.address_id = a.addressId;
